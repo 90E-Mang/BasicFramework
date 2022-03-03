c47f8f0e46e7d184ed895aaf187c6c18074b7363
90E-Mang <ggot2000@naver.com>
2022-03-03 15:34:41 +0900

    constructor

--- Ex15_Static_scope/Program.cs
+++ Ex15_Static_scope/Program.cs
@@ -28,14 +28,50 @@ namespace Ex15_Static_scope
             Console.WriteLine($"gMethod => i : {i}, j : {j}");
         }
     }
+
+    // 접근
+
+    class Test2
+    {
+        public static int sk;       // static variable      
+        public int gk;              // instance variable, member field
+
+        public void method()    // 일반함수
+        {   // sk(static) 변수에 접근이 가능한가 ? O
+            // gk(instance variable) 변수에 접근 가능한가? O
+            sk = 100;
+            gk = 200;
+        }
+
+        public static void sMethod()        // static 함수
+        {   // sk(static) 변수에 접근이 가능한가 ? O : 같은 식구는 static 서로 접근 가능
+            // gk(instance variable) 변수에 접근 가능한가? X : 항상 일반자원보다 static이 먼저 new를 하던 말던 안됨.
+            sk++;
+            // gk++;
+        }
+    }
     class Program
     {
+        private static int sint;
+        private int gint;
         static void Main(string[] args)
         {
             Test.method();
             //gMethod() 는 안됨 ... -> memory에 올려야댐.. -> new !
             Test t1 = new Test();
             t1.gMethod();       // 일반자원 사용
+
+            // 1. 일반함수에서는 : 일반자원, static 자원 접근 모두 가능함
+            // 2. static 함수에서는 : static 자원만 접근 가능하다.
+
+            // 여기서
+            // private static int sint;
+            // private int gint;
+            // 이 둘에게 값 할당 할려면?
+
+            Program.sint = 100; // 접근 가능
+            Program gm = new Program();
+            gm.gint = 100;
         }
     }
 }
