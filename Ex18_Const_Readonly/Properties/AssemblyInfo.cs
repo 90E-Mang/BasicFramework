f94c80504c58b48e853c333f8c610d9f92965927
90E-Mang <ggot2000@naver.com>
2022-03-04 11:23:43 +0900

    ㅇㅇㅇㅇ

--- Ex18_Const_Readonly/Properties/AssemblyInfo.cs
+++ Ex18_Const_Readonly/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("Ex08_Statement2")]
+[assembly: AssemblyTitle("Ex18_Const_Readonly")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ex08_Statement2")]
+[assembly: AssemblyProduct("Ex18_Const_Readonly")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("b6790629-f5d0-4c18-84a1-97d0ede5c937")]
+[assembly: Guid("9cb72dfe-0412-4b36-8c6c-8f1a61f0dd1f")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
