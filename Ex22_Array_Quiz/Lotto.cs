0800fa9e73ee2de391fca7822ef3ee27b788147a
90E-Mang <ggot2000@naver.com>
2022-03-04 19:45:07 +0900

    220304 수업

---
+++ Ex22_Array_Quiz/Lotto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex22_Array_Quiz
+{
+    class Lotto
+    {
+        private int[] numbers;
+        private Random random;
+
+        public Lotto()          // member field 초기화가 목적인 생성자에서 처리...
+        {
+            numbers = new int[6];   // 배열의 초기화(최초로 값을 갖는 행위)
+            random = new Random();
+        }
+
+        public void getReadLottoNumbers()
+        {
+
+        }
+        public void displayLottoNumbers()
+        {
+
+        }
+
+        // 추가적인 함수의 구현
+    }
+}
+/*
+ *  static void main(){
+ *  Lotto lotto = new Lotto();
+ *  lotto.getReadNumbers();
+ *  lotto.displayLottoNumbers();
+ * 
+ * 
+ * 
+ * }
+ */
