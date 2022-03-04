404d96d2d01020af2f2ad180f6a6d25989d121c3
90E-Mang <ggot2000@naver.com>
2022-03-04 10:14:47 +0900

    수업내용 커밋

---
+++ Ex20_Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex20_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 배열은 객체다
+            // 1. new 통해서 생성
+            // 2. heap 메모리에 생성
+            // 3. 고정배열(정적배열) : 배열의 크기가 한번 설정되면 변경 불가
+            // 4. 자료구조의 기본
+
+            int[] arr = new int[5];
+        }
+    }
+}
