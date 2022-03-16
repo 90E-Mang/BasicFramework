b353158ce7a536cae8f1f6323d427e9a0deb5c3b
90E-Mang <ggot2000@naver.com>
2022-03-16 19:32:26 +0900

    zz

---
+++ Ex25_String_class/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex25_String_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "가";
+            string b = "나";
+            string c = a + b;
+            Console.WriteLine($"C : {c}");
+
+            string[] strarr = new string[] {"가","나","다","라","마" };
+
+            foreach (string to in strarr)
+            {
+                Console.WriteLine($"to : {to}");
+            }
+
+            // 주의사항 : string은 int, double 처럼 사용하면 된다.(값타입 처럼)
+
+            string str = "가나다라마가나";
+            int loc = str.IndexOf("다");
+            Console.WriteLine($"loc : {loc}");
+
+            int lastloc = str.LastIndexOf("가");
+            Console.WriteLine($"lastloc : {lastloc}");
+
+            string strinsert = str.Insert(2,"H");
+            Console.WriteLine($"Insert : {strinsert}");
+
+            string strremove = str.Remove(0,3);
+            Console.WriteLine($"Remove : {strremove}");
+
+            string strreplace = str.Replace("라", "new");
+            Console.WriteLine($"strreplace : {strreplace}");
+
+            string strsubstring = str.Substring(0,1);   // 0,0을 넣으면 아무일도 일어나지 않는다 0,1을 써야 자기자신값
+            Console.WriteLine($"substring : {strsubstring}");
+
+            // 문제
+            string super = "슈퍼맨/팬티.노랑색/우하하-우하하";
+            // 슈퍼맨 팬티 노랑색 우하하 우하하 이 각각을 따로 추출 --> Split
+
+            char[] ch = new char[] {'/','.','-' };
+            string[] strarray = super.Split(ch);
+
+            foreach (var temp in strarray)
+            {
+                Console.WriteLine($"super array : {temp}");
+            }
+
+            int sum = 0;
+            string[] numarr = {"1","2","3","4","5" };
+            // 문제1) : 배열에 있는 문자값들의 합을 sum 변수에 담아서 출력; 결과 : 15
+
+            for (int i = 0; i < numarr.Length; i++)
+            {
+                sum += Convert.ToInt32(numarr[i]);
+            }
+            Console.WriteLine($"sum : {sum}");
+
+            string jumin = "123456-1234567";
+            int sum2 = 0;
+            //문제2) 주민번호의 합을 구하세요.(string은 char[] 이다)
+            // hint) jumin[i] >> return char >> 정수 >> 문자열 >> 정수
+            string[] juminsplit = jumin.Split('-');
+            for (int i = 0; i < jumin.Length; i++)
+            {
+                if (jumin[i].ToString() == "-")
+                {
+                    continue;
+                }
+                sum2 += int.Parse(jumin[i].ToString());
+            }
+
+            #region<듣기만하고 기억에서 삭제 !>
+
+            string strA = "hello";
+            Console.WriteLine(strA.GetHashCode());
+
+            string strB = "hello";
+            Console.WriteLine(strB.GetHashCode());
+            // 같은 주소
+
+            string strC = strA;
+            Console.WriteLine(strC.GetHashCode());
+            //////////////////////////////////////
+
+            strC = "World";
+            Console.WriteLine(strC.GetHashCode());
+
+            string strD = "가";
+            Console.WriteLine(strD.GetHashCode());
+            strD += "나";
+            Console.WriteLine(strD.GetHashCode());
+            strD += "다";
+            Console.WriteLine(strD.GetHashCode());
+            strD += "라";
+            Console.WriteLine(strD.GetHashCode());
+            strD += "마";
+            Console.WriteLine(strD.GetHashCode());
+
+            Console.WriteLine($"strD : {strD}");
+
+
+            // why ? 문자열의 누적 >> string 으로 하면 병신짓
+            // 문자열 누적할때는 얘를 써보자
+            StringBuilder sb = new StringBuilder();
+            sb.Append("가");
+            sb.Append("나");
+            sb.Append("다");
+
+            // 문자열의 누적 (데이터 양 많은 경우)
+
+            #endregion
+        }
+    }
+}
---
+++ Ex26_String_Function_Quiz/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex26_String_Function_Quiz
+{
+    // 주민번호 뒷 첫자리 1,3 -> 남자 2,4 여자라고 출력
+    // main 함수 주민번호 입력받고
+    // 입력값 : 123456-1234567
+
+    // 함수 3개를 static
+    // 1. 자리수 체크(기능)함수 (14 ok), return true, false
+    // 2. 뒷번호 첫번째 자리값 1~4까지의 값만 혀용 기능 함수 return true false
+    // 3. 뒷번호 첫번째 자리 값을 가지고 1,3 남자, 2,4 여자 출력 기능함수 void 출력
+    class Program
+    {
+        static void juminDisplay(string ssn)
+        {
+            switch (Convert.ToInt32(ssn[7].ToString()))
+            {
+                case 1:
+                case 3:
+                    Console.WriteLine("남자");
+                    break;
+                case 2:
+                case 4:
+                    Console.WriteLine("여자");
+                    break;
+            }               
+        }
+        static bool juminFirstNumber(string str)
+        {
+            if (Convert.ToInt32(str[7].ToString()) == 0)
+            {
+                return false;
+            }
+            return Convert.ToInt32(str[7].ToString()) > 5 ? false : true;
+        }
+        static bool juminCheck(string str)
+        {
+            return str.Length == 14 ? true : false;
+        }
+        static void Main(string[] args)
+        {
+            bool chk = true;
+            while (chk)
+            {
+                Console.Write("주민등록번호를 입력해주세요. : ");
+                string input = Console.ReadLine();
+
+                chk = juminCheck(input);
+                if (input.Length == 14)
+                {
+                    chk = juminFirstNumber(input);
+                    if (chk == true)
+                    {
+                        Console.WriteLine("정상입력");
+                    }
+                    else
+                    {
+                        Console.WriteLine("입력오류");
+                    }
+                    juminDisplay(input);
+                }
+                else
+                {
+                    Console.WriteLine("입력오류");
+                }
+                         
+            }
+            Console.WriteLine("프로그램 종료.");
+        }
+    }
+}
